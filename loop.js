52546ad88f7c8247838e85e7b1d38db2caa9479c
baliram123giri <giri710401@gmail.com>
2024-02-17 09:50:19 +0530

    loops done

--- loop.js
+++ loop.js
@@ -18,6 +18,44 @@
 //this loop itreate over the array 
 const newArr = [14, "5", { name: "trest" }, "Baliram", true, null, undefined, Symbol("test"), BigInt(45455444545), [], new Date()]
 
-newArr.forEach(function (divya) {
-    console.log(divya)
-})
+///map method
+// const mapTest = newArr.map((ele, index) => {
+//     if (ele === 14) {
+//         return "yes the value is 14"
+//     } else {
+//         return ele
+//     }
+// })
+// console.log(mapTest)
+// newArr.forEach(function (ele) {
+
+//     console.log(arrayData)
+
+// })
+const nameObj = { id: 1, name: "Baliram", pincode: 75254 }
+const test = "test"
+
+// for in
+// for (key in nameObj) {
+//     console.log(nameObj[key])
+// }
+
+// for (key of newArr) {
+//     console.log(key)
+// }
+
+//while loop
+// let i = 1
+// while (i < 10) {
+
+//     console.log(i)
+//     i++;
+// }
+
+//do while
+// let i = 0
+// do {
+//     i++
+//     console.log(i)
+// }
+// while (i < 10)
