401ef49995eefd7355e416da740b41517fbb24d5
baliram123giri <giri710401@gmail.com>
2024-02-23 09:20:45 +0530

    sort

---
+++ ArraySort.js
@@ -0,0 +1,31 @@
+//sorting is nothing but it's arranging the data in sequential manner.
+//what are the methods or sorting?
+//type of sorting
+//1) alphabetic
+//2) numeric
+
+//1)sort()
+//2) toSort()
+//3) reverse()
+//4) toReverse()
+
+let arr = [451, 457, 754, 4, 9045];
+// arr.sort((a, b) => b - a) //sort where  by default it give ascending order, if we compare with params in the callback function second param - first param = it will be in descending order
+// arr.toSorted((a, b) => b - a) it will give new array values
+// arr.reverse() IT WILL REVERSE TO THE ARRAY ELEMENTS
+// arr.toReversed() IT WILL REVERSE TO THE ARRAY ELEMENTS but with new array
+
+
+//numeric sorting
+//its independent module
+
+//1) random()
+//2) max()
+//3) min()
+
+//its belongs to Math module in js
+
+//  console.log(Math.random()) // it will give random numbers 
+
+//2) max()
+// console.log(Math.min(...arr))
